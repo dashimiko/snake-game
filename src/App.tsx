4c011daf2f8b916f69e757c4797ce0fb1e95e732
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-11-03 17:28:57 +0400

    added neon cursor component and start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.scss';
+import NeonCursor from './neonCursor';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <p className="glitch">
           Snake Game
         </p>
+        <button className="start_button">
+          Start
+        </button>
       </header>
+      <NeonCursor/>
     </div>
   );
 }
