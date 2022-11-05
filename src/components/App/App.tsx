9e74e8d722b40090ee04631723933036bcf85887
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-11-05 16:03:38 +0400

    new file structure + editorconfig

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../pages/index.scss';
+import NeonCursor from '../NeonCursor/NeonCursor';
+import Main from '../Main/Main';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Main/>
+      <NeonCursor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Main() {
+
+  return (
+    <main className="App_main">
+      <p className="glitch">
+        Snake Game
+      </p>
+      <button className="start_button">
+        Start
+      </button>
+    </main>
+  );
+}
+
+export default Main;
