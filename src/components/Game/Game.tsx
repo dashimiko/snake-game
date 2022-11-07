3966cb85b13b95eb10094ebe9c0fc5ce86ca581e
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-11-07 19:12:58 +0400

    first version with one cell

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,19 +1,70 @@
-import {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 
-function Game() {
+const Game = () => {
 
-  let canvasRef = useRef<HTMLCanvasElement | null>(null);
-  let canvasCtxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasCtxRef = useRef<CanvasRenderingContext2D | null>(null);
+
+  const [positionX, setPositionX] = useState(0);
+  const [positionY, setPositionY] = useState(0);
+
+  const [fieldCellX, setFieldCellX] = useState(15);
+  const [fieldCellY, setFieldCellY] = useState(0);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function moveSnake() {
+    setPositionX(positionX + fieldCellX);
+    setPositionY(positionY + fieldCellY);
+
+    if (positionX > 400) {
+      setPositionX(0);
+    } else if (positionX < 0) {
+      setPositionX(400);
+    }
+
+    if (positionY > 400) {
+      setPositionY(0);
+    } else if (positionY < 0) {
+      setPositionY(400);
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(moveSnake, 300);
+    return () => clearInterval(interval);
+  }, [moveSnake]);
 
   useEffect(() => {
     if (canvasRef.current) {
       canvasCtxRef.current = canvasRef.current.getContext('2d');
-      let ctx = canvasCtxRef.current;
+      const ctx = canvasCtxRef.current;
+      ctx!.clearRect(0, 0, window.innerWidth, window.innerHeight);
       ctx!.fillStyle = `#3FFE1A`;
-      ctx!.fillRect(0, 0, 20, 20);
-      console.log(ctx)
+      ctx!.fillRect(positionX, positionY, 20, 20);
     }
-  }, []);
+  }, [positionX, positionY]);
+
+document.addEventListener('keydown', function(event) {
+  if (event.code === "ArrowUp") {
+    setFieldCellY(-15);
+    setFieldCellX(0);
+  }
+
+  if (event.code === "ArrowDown") {
+    setFieldCellY(15);
+    setFieldCellX(0);
+  }
+
+  if (event.code === "ArrowLeft") {
+    setFieldCellX(-15);
+    setFieldCellY(0);
+  }
+
+  if (event.code === "ArrowRight") {
+    setFieldCellX(15);
+    setFieldCellY(0);
+  }
+});
 
   return (
     <div className="game_zone">
