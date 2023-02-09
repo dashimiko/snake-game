5a3f709f03468970bdb5a343c229012e731b735e
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-02-09 13:36:32 +0400

    aaaaaaaaaaaaa

--- src/components/Snake/Snake.ts
+++ src/components/Snake/Snake.ts
@@ -3,8 +3,9 @@ import {TsnakeSegment, TsnakeCoordinate, TsnakeDirection, ONE_CELL_SIZE} from '.
 export class Snake {
 
   snake: TsnakeSegment[];
-  position: TsnakeCoordinate;
-  direction: TsnakeDirection;
+  position: TsnakeCoordinate[];
+  direction: TsnakeDirection[];
+
 
   constructor(snake: TsnakeSegment[]) {
     this.snake = snake;
@@ -13,18 +14,21 @@ export class Snake {
   };
 
   moveSnake = () => {
-    //this.position = //тут хочется прибавить к
+
+    //this.position[0] = 1 + ONE_CELL_SIZE;
+    //тут хочется прибавить к
     //изначальной координате x змеи размер одной клетки
-    //но так не получается;
-    this._moveSnakeHead();
+    //но так не получается из-за типов;
+
+    //this._moveSnakeHead();
   };
 
-  _moveSnakeHead = () => {
+  /*_moveSnakeHead = () => {
     this.snake.unshift({
       position: this.position,
       direction: this.direction,
     });
 
     this.snake.pop();
-  };
+  };*/
 };
